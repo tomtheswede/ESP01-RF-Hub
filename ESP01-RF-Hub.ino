2609113231b5c65c4c4ff0762c07ca6b7c1adcb4
Thomas Friberg <tomtheswede@gmail.com>
2016-10-09 09:53:01 +0800

    Now reading RF signals
    RF signals have to be properly pulsed to be read.

--- ESP01-RF-Hub.ino
+++ ESP01-RF-Hub.ino
@@ -1,66 +1,86 @@
-byte buf[10] = {0,0,0,0,0,0,0,0,0,0};
+byte buf[8] = {0,0,0,0,0,0,0,0};
 int pingBuf[100];
+long lastRead=0;
+long preRead=0;
 long ms1;
 long ms2;
-int readPin = 4;
+long ms3;
+int readPin = 2;
 int pingCount = 0;
-int pingNum = 0;
+int storeNum = 0;
 String out[14] = {",","1","2","3","4","5","6","7","8","9","0",".","e","s"};
 bool record=false;
 bool checkOut=false;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (int i=0; i<9; i++) {
-    buf[i]=buf[i+1];
+  preRead=micros();
+  if (micros()-lastRead>10) { //sets a minimum re-trigger time for the check of - this if statement and loop takes 3 microseconds
+    // update buffer with 8 most recent values
+    for (int i=0; i<7; i++) {
+      buf[i]=buf[i+1];
+    }
+    buf[7]=digitalRead(readPin);
+    lastRead=micros();
   }
-  buf[9]=digitalRead(readPin);
-  if (buf[0]==1 && buf[1]==1 && buf[2]==0 && buf[3]==0) {
+  //600 & 500 work well,
+  if (micros()-ms1>400 && buf[0]==0 && buf[1]==0 && buf[2]==0 && buf[3]==0 && buf[4]==1 && buf[5]==1 && buf[6]==1 && buf[7]==1) { //start timer for rise
     ms1=micros();
-  }
-  if (buf[0]==0 && buf[1]==0 && buf[2]==1 && buf[3]==1) {
-    ms2=micros();
     pingCount++;
   }
-  
-  if (ms2-ms1>320) { //between 270 and 400 works - 350 is good
-    //Serial.println(pingCount);
-    if (pingCount==14 && not(record)) { //Start message
-      record=true;
-      pingNum=0;
-    }
-    if (record) {
-      pingBuf[pingNum]=pingCount;
-      pingNum++;
-    }
-    if (pingCount==13 && record) { //end message and dump command
-      record=false;
-      checkOut=true;
-      clearBuf();
-    }
-    ms1=ms2;
+  if (pingCount>14) {
+    //Serial.println("pingCount Buffer exceeded");
     pingCount=0;
   }
+  //1300 works well
+  if (micros()-ms1>1300 && pingCount>0) { //between 270 and 400 works - 350 is good
+    //Serial.println(pingCount); //This needs to be removed for correct timing
+    if (pingCount==14 && not(record)) { //Start message
+      //Serial.println("Start message"); //This needs to be removed for correct timing
+      record=true;
+    }
+    if (record) {
+      pingBuf[storeNum]=pingCount;
+      //Serial.print("store"); //This needs to be removed for correct timing
+      storeNum++;
+      if (storeNum==20) {
+        pingCount=13;
+      }
+    }
+    if (pingCount==13 && record) { //end message and dump command
+      //Serial.println("Stop record"); //This needs to be removed for correct timing
+      record=false;
+      checkOut=true;
+    }
+    pingCount=0;
+    //ms3=micros();
+    //Serial.print(ms3-ms2);
+    //Serial.println(" micros for execution");
+  }
+  if (micros()-ms1>30000) {
+    ms1=micros();
+  }
 
-  if (checkOut) {
-    for (int i=0; i<pingNum; i++) {
-      Serial.print(out[pingBuf[i]-1]);
+  if (checkOut && storeNum>2) {
+    for (int i=0; i<storeNum; i++) {
+      if (pingBuf[i]<=14) {
+        Serial.print(out[pingBuf[i]-1]);
+      }
       if (pingBuf[i]==13) {
         Serial.println("");
       }
     }
     checkOut=false;
+    storeNum=0;
   }
-  delayMicroseconds(10);
+  //ms3=micros();
+  //Serial.print(ms3-preRead);
+  //Serial.println(" time for loop");
+
 }
 
-void clearBuf() {
-  for (int i=0; i<10; i++) {
-    buf[i]=0;
-  }
-}
+
