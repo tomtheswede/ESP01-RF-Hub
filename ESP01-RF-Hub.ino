4f7eb1307bf42044b38213d18c0726c15a950d2c
Thomas Friberg <tomtheswede@gmail.com>
2016-10-12 22:36:40 +0800

    Includes the function command

--- ESP01-RF-Hub.ino
+++ ESP01-RF-Hub.ino
@@ -1,3 +1,10 @@
+// Import ESP8266 libraries
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+const char* sensorID1 = "BUT007"; //Name of sensor
+const char* deviceDescription = "TestButton";
+
 byte buf[8] = {0,0,0,0,0,0,0,0};
 int pingBuf[100];
 long lastRead=0;
@@ -8,18 +15,51 @@ long ms3;
 int readPin = 2;
 int pingCount = 0;
 int storeNum = 0;
-String out[14] = {",","1","2","3","4","5","6","7","8","9","0",".","e","s"};
+String out[15] = {",","1","2","3","4","5","6","7","8","9","0",".","f","e","s"};
 bool record=false;
 bool checkOut=false;
+String addString="";
+
+// WiFi parameters
+const char* ssid = ""; //Enter your WiFi network name here in the quotation marks
+const char* password = ""; //Enter your WiFi pasword here in the quotation marks
+
+//Server details
+unsigned int localPort = 5007;  //UDP send port
+const char* ipAdd = "192.168.0.100"; //Server address
+byte packetBuffer[512]; //buffer for incoming packets
+
+WiFiUDP Udp; //Instance to send packets
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected with IP: ");
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+
+  //Open the UDP monitoring port
+  Udp.begin(localPort);
+  Serial.print("Udp server started at port: ");
+  Serial.println(localPort);
+
+  delay(2000); //Time clearance to ensure registration
+  SendUdpValue("REG",sensorID1,String(deviceDescription)); //Register LED on server
+  //delay(2000); //Time clearance to ensure registration
+  //SendUdpValue("REG",sensorID1,String(deviceDescription)); //Register LED on server
 }
 
 void loop() {
   preRead=micros();
-  if (micros()-lastRead>10) { //sets a minimum re-trigger time for the check of - this if statement and loop takes 3 microseconds
+  if (micros()-lastRead>20) { //sets a minimum re-trigger time for the check of - this if statement and loop takes 3 microseconds
     // update buffer with 8 most recent values
     for (int i=0; i<7; i++) {
       buf[i]=buf[i+1];
@@ -28,18 +68,18 @@ void loop() {
     lastRead=micros();
   }
   //600 & 500 work well,
-  if (micros()-ms1>400 && buf[0]==0 && buf[1]==0 && buf[2]==0 && buf[3]==0 && buf[4]==1 && buf[5]==1 && buf[6]==1 && buf[7]==1) { //start timer for rise
+  if (micros()-ms1>860 && buf[0]==0 && buf[1]==0 && buf[2]==0 && buf[3]==1 && buf[4]==1 && buf[5]==1 && buf[6]==1 && buf[7]==1) { //start timer for rise
     ms1=micros();
     pingCount++;
   }
-  if (pingCount>14) {
+  if (pingCount>15) {
     //Serial.println("pingCount Buffer exceeded");
     pingCount=0;
   }
   //1300 works well
-  if (micros()-ms1>1300 && pingCount>0) { //between 270 and 400 works - 350 is good
+  if (micros()-ms1>1600 && pingCount>0) { //between 270 and 400 works - 350 is good
     //Serial.println(pingCount); //This needs to be removed for correct timing
-    if (pingCount==14 && not(record)) { //Start message
+    if (pingCount==15 && not(record)) { //Start message
       //Serial.println("Start message"); //This needs to be removed for correct timing
       record=true;
     }
@@ -48,39 +88,53 @@ void loop() {
       //Serial.print("store"); //This needs to be removed for correct timing
       storeNum++;
       if (storeNum==20) {
-        pingCount=13;
+        pingCount=14;
       }
     }
-    if (pingCount==13 && record) { //end message and dump command
+    if (pingCount==14 && record) { //end message and dump command
       //Serial.println("Stop record"); //This needs to be removed for correct timing
       record=false;
       checkOut=true;
     }
     pingCount=0;
-    //ms3=micros();
-    //Serial.print(ms3-ms2);
-    //Serial.println(" micros for execution");
+
   }
+  
   if (micros()-ms1>30000) {
     ms1=micros();
   }
 
   if (checkOut && storeNum>2) {
+    addString="";
     for (int i=0; i<storeNum; i++) {
-      if (pingBuf[i]<=14) {
-        Serial.print(out[pingBuf[i]-1]);
-      }
-      if (pingBuf[i]==13) {
-        Serial.println("");
+      if (pingBuf[i]<=13) {
+        addString=addString+out[pingBuf[i]-1];
       }
     }
+    Serial.println(addString);
     checkOut=false;
     storeNum=0;
+    if (addString=="3,2") {
+      SendUdpValue("LOG",sensorID1,"toggle");
+    }
   }
-  //ms3=micros();
-  //Serial.print(ms3-preRead);
-  //Serial.println(" time for loop");
+    
+  
+}
 
+void SendUdpValue(String type, String sensorID, String value) {
+  //Print GPIO state in serial
+  Serial.print("-Value sent via UDP: ");
+  Serial.println(type + "," + sensorID + "," + value);
+
+  // send a message, to the IP address and port
+  Udp.beginPacket(ipAdd,localPort);
+  Udp.print(type);
+  Udp.write(",");
+  Udp.print(sensorID);
+  Udp.write(",");
+  Udp.print(value); //This is the value to be sent
+  Udp.endPacket();
 }
 
 
