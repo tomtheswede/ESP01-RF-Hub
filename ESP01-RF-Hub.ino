fc06d5519a19928957648db59dd7418dd95dd89e
Thomas Friberg <tomtheswede@gmail.com>
2016-12-05 20:27:49 +0800

    Simplification and addition of new sensor IDs

--- ESP01-RF-Hub.ino
+++ ESP01-RF-Hub.ino
@@ -4,8 +4,10 @@
 
 const char* sensorID1 = "BUT008"; //Name of sensor
 const char* sensorID2 = "BUT009"; //Name of sensor
+const char* sensorID3 = "BUT010"; //Name of sensor
 const char* deviceDescription1 = "RadioButton1";
 const char* deviceDescription2 = "RadioButton2";
+const char* deviceDescription3 = "RadioButton3";
 
 byte niblett[4];
 int nibNum;
@@ -17,11 +19,13 @@ long fallLen=0;
 long riseLen=0;
 long startTime=0;
 int readPin = 2;
+int indicatorPin = 14; //temp for debugging
 int readState=0;
 int pingCount = 0;
 int storeNum = 0;
 String out[16] = {"0","1","2","3","4","5","6","7","8","9",".",",","e","p","f","s"};
 bool record=false;
+int highBits=0;
 String addString="";
 
 // WiFi parameters
@@ -56,54 +60,68 @@ void setup() {
   Serial.println(localPort);
 
   delay(2000); //Time clearance to ensure registration
+  pinMode(indicatorPin,OUTPUT);
   SendUdpValue("REG",sensorID1,String(deviceDescription1)); //Register LED on server
   SendUdpValue("REG",sensorID2,String(deviceDescription2)); //Register LED on server
+  SendUdpValue("REG",sensorID3,String(deviceDescription3)); //Register LED on server
   
   attachInterrupt(digitalPinToInterrupt(readPin),changeInterrupt,CHANGE); //Comment out to remove button functionality
 }
 
 void loop() {
-  if ((riseFlag || fallFlag) && (riseLen<200 || fallLen<200 || riseLen>=900 || fallLen>=900)) { //Noise filter
+  //If a byte has finished sending, the riseFlag will fire
+  if (riseFlag && riseLen>200 && riseLen<520 && fallLen>530 && fallLen<810) { //high low low - 0
+    niblett[0]=niblett[1];
+    niblett[1]=niblett[2];
+    niblett[2]=niblett[3];
+    niblett[3]=0;
     riseFlag=false;
-    fallFlag=false;
-    pingCount=0;
+    pingCount++;
   }
-  else if (riseFlag) {
+  else if (riseFlag && riseLen>530 && riseLen<810 && fallLen>200 && fallLen<520) { //high high low - 1
+    niblett[0]=niblett[1];
+    niblett[1]=niblett[2];
+    niblett[2]=niblett[3];
+    niblett[3]=1;
+    highBits=highBits+1; //for parity calculation
     riseFlag=false;
-    fallFlag=false;
-    if (riseLen>=200 && riseLen<500 && fallLen>=500 && fallLen<900) {
-      niblett[0]=niblett[1];
-      niblett[1]=niblett[2];
-      niblett[2]=niblett[3];
-      niblett[3]=0;
-      pingCount++;
+    pingCount++;
+  }
+  else if (riseFlag) { //If a signal has a bad bit, start again.
+    riseFlag=false;
+    pingCount=0;
+    highBits=0;
+    if (addString.length()>0) {
+      checkOut(addString); //For debugging - detecting half-messages
     }
-    else if (riseLen>=500 && riseLen<900 && fallLen>=200 && fallLen<500) {
-      niblett[0]=niblett[1];
-      niblett[1]=niblett[2];
-      niblett[2]=niblett[3];
-      niblett[3]=1;
-      pingCount++;
-    }
-    else {
-      pingCount=0;
-    }
+    addString="";
+    record=false;
+    digitalWrite(indicatorPin,LOW);
   }
 
   if (niblett[0]==1 && niblett[1]==1 && niblett[2]==1 && niblett[3]==1 && !record) {
-    record=true;
+    record=true; //Start recording
+    digitalWrite(indicatorPin,HIGH);
     pingCount=0;
+    highBits=0;
     startTime=millis();
   }
   else if (pingCount==4 && record) {
-    if (niblett[0]==1 && niblett[1]==1 && niblett[2]==0 && niblett[3]==1 && addString.length()>0) { //end recording
-      checkOut(addString);
+    if (niblett[0]==1 && niblett[1]==1 && niblett[2]==0 && (niblett[3]==1 || niblett[3]==0) && addString.length()>0) { //end recording with parity bit
+      if ((highBits)%2) { //only even parity gets printed. Otherwise, in error
+        checkOut(addString);
+      }
+      else {
+        Serial.println("Parity fail");
+      }
       addString="";
-      record=false;
+      record=false; //End recording
+      digitalWrite(indicatorPin,LOW);
     }
     else if (millis()-startTime>200) {
       addString="";
       record=false;
+      digitalWrite(indicatorPin,LOW);
     }
     else {
       nibNum=niblett[0]*8+niblett[1]*4+niblett[2]*2+niblett[3];
@@ -154,6 +172,20 @@ void checkOut(String message) {
   else if (message=="1,1,4,4") {
     SendUdpValue("LOG",sensorID2,"longestPress");
   }
+  else if (message=="1,1,5,1") {
+    //SendUdpValue("LOG",sensorID3,"press");
+    Serial.println("Success");
+  }
+  else if (message=="1,1,5,2") {
+    SendUdpValue("LOG",sensorID3,"longPress");
+  }
+  else if (message=="1,1,5,3") {
+    SendUdpValue("LOG",sensorID3,"longerPress");
+  }
+  else if (message=="1,1,5,4") {
+    SendUdpValue("LOG",sensorID3,"longestPress");
+  }
+  //delay(50); //To avoid double message counting due to RF signal redundancy
 }
 
 void SendUdpValue(String type, String sensorID, String value) {
